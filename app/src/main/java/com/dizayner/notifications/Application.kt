f738996df01ca4d6a27f1a8af0122770c8d9f745
dizayn3r <dizayn3r@gmail.com>
2022-07-30 20:39:02 +0530

    Initial commit

---
+++ app/src/main/java/com/dizayner/notifications/Application.kt
@@ -0,0 +1,58 @@
+package com.dizayner.notifications
+
+import android.app.*
+import android.app.Application
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.res.ResourcesCompat
+
+
+private const val CHANNEL_ID = "heads_up_alerts"
+private const val CHANNEL_NAME ="Heads Up Alerts"
+
+class Application : Application() {
+    private val notificationManager by lazy { NotificationManagerCompat.from(this)}
+
+    override fun onCreate(){
+        super.onCreate()
+        createNotificationChannel()
+        notificationManager.notify(1,createNotification())
+    }
+
+    private fun createNotificationChannel(){
+        if (Build.VERSION.SDK_INT<Build.VERSION_CODES.O) return
+        val  channel = NotificationChannel (
+            CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH
+                ).apply {
+                    lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+        }
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    private fun createNotification():Notification{
+        val  contentIntent = Intent(this,HomeActivity::class.java)
+        val contentPendingIntent = PendingIntent.getActivity(this, 0, contentIntent, 0)
+        val  fullScreenIntent = Intent(this,LockscreenActivity::class.java)
+        val fullScreenPendingIntent = PendingIntent.getActivity(this, 0, fullScreenIntent,0)
+
+        return NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.notifications_active_black_24)
+            .setColor(ResourcesCompat.getColor(resources,R.color.purple_200,null))
+            .setContentTitle("Heads Up Notification")
+            .setAutoCancel(true)
+            .setContentIntent(contentPendingIntent)
+            .setFullScreenIntent(fullScreenPendingIntent, true)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setCategory(NotificationCompat.CATEGORY_ALARM)
+            .build()
+
+
+
+    }
+
+
+
+
+}
---
+++ app/src/main/java/presentation/LockscreenActivity.kt
@@ -0,0 +1,30 @@
+package com.dizayner.notifications
+
+import android.os.Bundle
+import android.os.Build
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.dizayner.notifications.databinding.ActivityLockscreenBinding
+
+class LockscreenActivity :AppCompatActivity(){
+    private lateinit var binding: ActivityLockscreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        showOnLockScreenAndTurnScreenOn()
+
+        binding = ActivityLockscreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    private fun showOnLockScreenAndTurnScreenOn(){
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.O_MR1){
+            setShowWhenLocked(true)
+            setTurnScreenOn(true)
+        }else{
+            window.addFlags(
+                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+            )
+        }
+    }
+
+}
